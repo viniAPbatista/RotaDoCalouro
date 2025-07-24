1c2ab9e1057963de52579420a2c2313661b88edb
Vinicius Batista <vini.A.P.batista@gmail.com>
2025-07-24 18:19:51 -0300

    pagina criar post iniciada

--- src/app/(protected)/(tabs)/feed.tsx
+++ src/app/(protected)/(tabs)/feed.tsx
@@ -1,13 +1,24 @@
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList, TouchableOpacity, Text } from "react-native";
 import PostListItem from "@/src/components/postListItem";
+import { useRouter } from "expo-router";
 
 export default function Feed() {
+
+   const router = useRouter()
+
+  function handleAcessCriarPost() {
+      router.push('/criarPost')
+    }
+
   return (
-    <View>
+    <View style={styles.container}>
       {/* <FlatList
         data={}
         renderItem={({ item }) => <PostListItem post={item} />}
       /> */}
+      <TouchableOpacity style={styles.ButtonAdicionarPost} onPress={handleAcessCriarPost}>
+        <Text style={styles.TextAdicionarPost}>+</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -18,5 +29,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#C2DCF2',
     paddingTop: 32,
     paddingHorizontal: 16,
+  },
+  ButtonAdicionarPost: {
+    position: 'absolute',
+    bottom: 130,
+    right: 30,
+    backgroundColor: '#272874ff',
+    borderRadius: 30,
+    width: '15%',
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  TextAdicionarPost: {
+    color: 'white',
+    fontSize: 35,
   },
 });
---
+++ src/app/(protected)/criarPost.tsx
@@ -0,0 +1,90 @@
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+
+export default function CriarPost() {
+
+    const [textPost, setTextPost] = useState('')
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}
+            >
+                <View style={styles.containerHeader}>
+                    <Text style={styles.message}>Criar Post</Text>
+                </View>
+
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                    <View style={styles.containerForm}>
+                        <Text style={styles.title}>Publique algo!</Text>
+                        <TextInput
+                            placeholder="No que você está pensando..."
+                            style={styles.input}
+                            value={textPost}
+                            onChangeText={setTextPost}
+                            multiline
+                            textAlignVertical="top" // para alinhar o texto no topo
+                        />
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>Publicar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#C2DCF2',
+    },
+    containerHeader: {
+        marginTop: '5%',
+        marginBottom: '4%',
+        paddingStart: '5%'
+    },
+    message: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    containerForm: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%',
+        paddingBottom: 20,
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 28,
+        marginBottom: 10,
+    },
+    input: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        padding: 12,
+        borderColor: '#ccc',
+        minHeight: 100,
+    },
+    button: {
+        backgroundColor: '#272874ff',
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 12,
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+})
